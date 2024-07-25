fc581e593562e83ad938513e41a6144a4bfca0d3
haisom2022 <103404415+haisom2022@users.noreply.github.com>
2024-07-25 15:53:52 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Clouddrive-ChatGPT",
+  description: "ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "Clouddrive-ChatGPT",
     statusBarStyle: "default",
   },
 };
